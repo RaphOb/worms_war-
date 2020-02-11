c54f8bac302632d72f442772766c03c6b75bddf4
Geoffrey <geoffrey.bukin@hotmail.fr>
2020-02-11 09:07:21 +0100

    Setup SFML

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <SFML/Graphics.hpp>
+#include <unistd.h>
+#include "src/Tilemap.hh"
+
+int main()
+{
+
+    int height = 8;
+    int width = 16;
+    int tileWidth = 32;
+    int tileHeight = 32;
+    sf::RenderWindow window(sf::VideoMode(tileWidth * width, tileHeight * height), "SFML works!");
+
+    const int level[] =
+            {
+                    0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+                    1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+                    0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+                    0, 1, 1, 1, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+                    0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+                    2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+                    0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+            };
+
+    Tilemap map;
+
+    if (!map.load("../resources/tilemap.png", sf::Vector2u(tileWidth, tileHeight), level, width, height))
+        return -1;
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(map);
+        window.display();
+    }
+
+    return 0;
+}
