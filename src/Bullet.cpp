79c7e42131221e97e4ad9bd08ae164e7e2745dab
Raph <obadia_r@etna-alternance.net>
2020-02-19 11:19:31 +0100

    class bullet

---
+++ src/Bullet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by oraph on 19/02/2020.
+//
+
+#include "Bullet.hh"
---
+++ src/Bullet.hh
@@ -0,0 +1,14 @@
+//
+// Created by oraph on 19/02/2020.
+//
+
+#ifndef LITTLEBIGGAME_BULLET_HH
+#define LITTLEBIGGAME_BULLET_HH
+
+
+class Bullet {
+
+};
+
+
+#endif //LITTLEBIGGAME_BULLET_HH
