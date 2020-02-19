865cb1a269127b9662fa8296fbcc506226377f55
Raph <obadia_r@etna-alternance.net>
2020-02-19 16:50:45 +0100

    fix todo and fix sprite bazooka

--- src/Worm.cpp
+++ src/Worm.cpp
@@ -17,6 +17,7 @@ Worm::Worm(AnimatedSprite animatedSprite, std::vector<Animation> animations) : m
     m_animatedSprite.setOrigin(m_currentAnimation->getFrame(0).width / 2, m_currentAnimation->getFrame(0).height / 2);
     Worm::textureLoad();
     sprite.setTexture(bazookaTexture);
+    sprite.setTextureRect(sf::IntRect(0, 0, 55, 34));
 }
 
 sf::Vector2f Worm::getVelocity() const {
@@ -32,15 +33,6 @@ void Worm::setYVelocity(float v) {
 }
 
 void Worm::draw(sf::RenderWindow &window) {
-    // TODO a deplacer dans update
-    sprite.setPosition(sf::Vector2f(Worm::getPosition().x - 45, Worm::getPosition().y - 15));
-    if(leftorright == 0){
-    sprite.setPosition(sf::Vector2f(Worm::getPosition().x - 45, Worm::getPosition().y - 15));
-    } else if (leftorright == 1) {
-        sprite.setPosition(sf::Vector2f(Worm::getPosition().x - 10, Worm::getPosition().y - 15));
-    }
-
-
     window.draw(sprite);
     if (bullet) bullet->draw(window);
     window.draw(m_animatedSprite);
@@ -101,6 +93,15 @@ void Worm::update(sf::Time frameTime, sf::RenderWindow& window) {
     if(hasshot) {
         bullet->update(frameTime);
     }
+    // update pos bazooka
+    sprite.setPosition(sf::Vector2f(Worm::getPosition().x - 45, Worm::getPosition().y - 15));
+    if(leftorright == 0){
+        sprite.setPosition(sf::Vector2f(Worm::getPosition().x - 45, Worm::getPosition().y - 15));
+    } else if (leftorright == 1) {
+        sprite.setPosition(sf::Vector2f(Worm::getPosition().x - 10, Worm::getPosition().y - 15));
+    }
+    // end update pos bazooka
+
     m_velocity.y += 981.f * frameTime.asSeconds();
 
     m_animatedSprite.play(*m_currentAnimation);
