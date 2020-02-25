19e457e13751a854eb37e389bf2e89165c24fcd4
Raph <obadia_r@etna-alternance.net>
2020-02-25 21:36:27 +0100

    scene et boom

---
+++ src/Scenes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by oraph on 25/02/2020.
+//
+
+#include "Scenes.hh"
---
+++ src/Scenes.hh
@@ -0,0 +1,14 @@
+//
+// Created by oraph on 25/02/2020.
+//
+
+#ifndef LITTLEBIGGAME_SCENES_HH
+#define LITTLEBIGGAME_SCENES_HH
+
+
+class Scenes {
+
+};
+
+
+#endif //LITTLEBIGGAME_SCENES_HH
